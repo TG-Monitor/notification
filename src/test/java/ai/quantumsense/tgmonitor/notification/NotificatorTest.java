338072910226423142121b4180f438345c897daa
Daniel Weibel <danielmweibel@gmail.com>
2018-07-12 18:30:40 +0800

    Update 'backend' dep and use newest version of 'monitor'

--- src/test/java/ai/quantumsense/tgmonitor/notification/NotificatorTest.java
+++ src/test/java/ai/quantumsense/tgmonitor/notification/NotificatorTest.java
@@ -25,15 +25,39 @@ public class NotificatorTest extends AbsTest {
             @Override
             public void setPeers(Set<String> set) { }
             @Override
+            public void addPeer(String s) {}
+            @Override
+            public void addPeers(Set<String> set) {}
+            @Override
+            public void removePeer(String s) {}
+            @Override
+            public void removePeers(Set<String> set) {}
+            @Override
             public Set<String> getPatterns() { return null; }
             @Override
             public void setPatterns(Set<String> set) { }
+            @Override
+            public void addPattern(String s) {}
+            @Override
+            public void addPatterns(Set<String> set) {}
+            @Override
+            public void removePattern(String s) {}
+            @Override
+            public void removePatterns(Set<String> set) {}
             @Override
             public Set<String> getEmails() {
                 return new HashSet<>(Arrays.asList("danielmweibel@gmail.com", "danielweibel@gmx.ch"));
             }
             @Override
             public void setEmails(Set<String> set) { }
+            @Override
+            public void addEmail(String s) {}
+            @Override
+            public void addEmails(Set<String> set) {}
+            @Override
+            public void removeEmail(String s) {}
+            @Override
+            public void removeEmails(Set<String> set) { }
         };
         notificator = new NotificatorImpl(formatter, sender, monitorDataFactory);
     }
